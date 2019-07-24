0c776232729d4701a33bd18e158f9b8d81ab3fec
sophieqzwang <sophieqzwang@gmail.com>
2019-07-24 16:18:24 -0400

    Local change to 30

--- TestMain.m
+++ TestMain.m
@@ -1,10 +1,10 @@
 % @Author: Sophie Calder-Wang
 % @Date:   2019-07-24 15:10:12
 % @Last Modified by:   Sophie Calder-Wang
-% @Last Modified time: 2019-07-24 15:42:27
+% @Last Modified time: 2019-07-24 16:18:13
 
 x = 2;
-y = 15;
+y = 30;
 
 z = x + y;
 fprintf('x + y = %d \n', z) ;
