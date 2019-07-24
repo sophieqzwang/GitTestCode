0d193b35978a0a164c9816857f6da0ef65b135d5
sophieqzwang <sophieqzwang@gmail.com>
2019-07-24 17:14:39 -0400

    Commit local change

--- TestMain.m
+++ TestMain.m
@@ -1,10 +1,12 @@
 % @Author: Sophie Calder-Wang
 % @Date:   2019-07-24 15:10:12
 % @Last Modified by:   Sophie Calder-Wang
-% @Last Modified time: 2019-07-24 16:19:45
+% @Last Modified time: 2019-07-24 17:14:20
 
 x = 2;
 y = 30;
 
 z = x + y;
 fprintf('x + y = %d \n', z) ;
+
+fprintf('Hello Sophie');
