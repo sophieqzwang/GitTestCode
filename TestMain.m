d0f9cdb147cc61c454cce5adf4f88f83a7896de4
sophieqzwang <sophieqzwang@gmail.com>
2019-08-09 09:55:19 -0400

    sth

--- TestMain.m
+++ TestMain.m
@@ -1,7 +1,7 @@
 % @Author: Sophie Calder-Wang
 % @Date:   2019-07-24 15:10:12
 % @Last Modified by:   Sophie Calder-Wang
-% @Last Modified time: 2019-08-09 09:42:05
+% @Last Modified time: 2019-08-09 09:54:35
 
 x = 2;
 y = 30;
@@ -11,4 +11,5 @@ fprintf('x + y = %d \n', z) ;
 
 fprintf('Hello Sophie');
 fprintf('Hello World');
+fprintf('Trying to commit');
 fprintf('TGIF');
